8c3a6e830a1bad1023d94cb3ea4c1862d25022cd
bartoszwmalec <bartosz.malec@yahoo.pl>
2026-07-08 15:30:45 +0200

    Add SQL query for user data with metadata
    Added SQL query to select user data with metadata columns.

--- read_files.py
+++ read_files.py
@@ -23,5 +23,19 @@ FROM STREAM read_files(
   header => true
 )
 """
+spark.sql(query)
 
+# file "_metadata" columns (ref.: docs.databricks.com/aws/en/ingestion/file-metadata-column)
+query = """
+SELECT
+  *,
+  cast(from_unixtime(user_first_touch_timestamp / 1000000) AS DATE) AS first_touch_date,
+  _metadata.file_modification_time AS file_modification_time,   -- Last data source file modification time
+  _metadata.file_name AS source_file,                            -- Ingest data source file name
+  current_timestamp() as ingestion_time                          -- Ingestion timestamp
+FROM read_files(
+  "/Volumes/dbacademy_ecommerce/v01/raw/users-historical",
+  format => 'parquet')
+LIMIT 10;
+"""
 spark.sql(query)
